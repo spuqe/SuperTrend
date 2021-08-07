8e39b2dd270fc1acb80eb4b5e06efcb57720b573
Hui Liao <CryT0r@protonmail.com>
2021-08-07 19:21:53 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BINANCE_API_KEY = 'BINANCEAPIKEY' # ¤ Your Binance API key
+BINANCE_SECRET_KEY = 'BINANCESECRETKEY' # ¤ Your Binance Secret Key
+
+# Where to find Binance API and secret key?
+# https://www.binance.com/en/my/settings/api-management
